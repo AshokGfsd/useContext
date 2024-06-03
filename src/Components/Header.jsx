2cbed7e510168ba0c3de822c4537a8ac7476f0d1
AshokGfsd <166788378+AshokGfsd@users.noreply.github.com>
2024-06-03 12:11:55 +0530

    first commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { useUserContext } from "../ContextProvider";
 
 function Header() {
-  const { cardQuantity, cardAmount, clear,setClear,TotalCard } = useUserContext();
+  const { cardQuantity, cardAmount, clear,setClear,TotalCard,setTotalcard } = useUserContext();
 
   const handleClear = () => {
     setClear(true)
+    setTotalcard (false)
   };
   return (
     <>
-      {TotalCard&&!clear&&<header className="hide">
+      {<header className={(!TotalCard)?"hide":"show"}>
         <div className="row bg-light d-flex justify-content-between align-items-center py-3">
           <div className="col">TOTAL QTY: {cardQuantity}</div>
           <div className="col">TOTAL Price : ${cardAmount.toFixed(2)}</div>
